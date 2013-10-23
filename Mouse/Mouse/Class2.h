6f0ca646ff88dd96597cb948c414d222e7b0c597
Tushar Vengurlekar <tushar@creativecapsule.com>
2013-10-23 19:35:31 +0530

    5th Commit

---
+++ Mouse/Mouse/Class2.h
@@ -0,0 +1,13 @@
+//
+//  Class2.h
+//  Mouse
+//
+//  Created by Tushar on 23/10/13.
+//  Copyright (c) 2013 creativecapsule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Class2 : NSObject
+
+@end
---
+++ Mouse/Mouse/Class2.m
@@ -0,0 +1,13 @@
+//
+//  Class2.m
+//  Mouse
+//
+//  Created by Tushar on 23/10/13.
+//  Copyright (c) 2013 creativecapsule. All rights reserved.
+//
+
+#import "Class2.h"
+
+@implementation Class2
+
+@end
